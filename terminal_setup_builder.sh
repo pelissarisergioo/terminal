097788e381f4ef8855f250f125aef25ce64ceae9
Sergio Pelissari <sonared@gmail.com>
2019-03-17 21:14:01 +0100

    Adding the builder script

---
+++ terminal_setup_builder.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "[+] Removing some previous installation...."
+rm -rf ~/.vim
+
+echo "[+] Installing Bundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "[+] Generating the .vimrc file...."
+cat >~/.vimrc <<EOL
+set nocompatible              " be iMproved, required
+filetype off                  " required
+syntax on
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'tpope/vim-fugitive'
+Plugin 'davidhalter/jedi-vim'
+Plugin 'scrooloose/nerdtree'
+Plugin 'jnurmine/Zenburn'
+Plugin 'altercation/vim-colors-solarized'
+
+call vundle#end()            " required
+filetype plugin indent on    " required
+
+inoremap " ""<left>
+inoremap ' ''<left>
+inoremap ( ()<left>
+inoremap [ []<left>
+inoremap { {}<left>
+inoremap {<CR> {<CR>}<ESC>O
+inoremap {;<CR> {<CR>};<ESC>O
+nnoremap <buffer> <C-r> <Esc>:w<CR>:exec '!python3' shellescape(@%, 1)<cr>
+
+if has('gui_running')
+  set background=dark
+  colorscheme solarized
+else
+  colorscheme zenburn
+endif
+
+set nu
+
+"NerdTree Setup"
+let NERDTreeIgnore=['\.pyc$', '\~$'] "ignore files in NERDTree
+map <C-n> :NERDTreeToggle<CR>
+
+let python_highlight_all=1
+EOL
+
+echo "[+] Running VIM just to install the Required plugins..."
+vim -c ':PluginInstall'
