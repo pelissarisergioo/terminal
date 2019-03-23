62fb2bd1ad52429b5f576b5ac258f9a3ef918ffb
Sérgio Pelissari <sergio.pelissari@netshoes.com>
2019-03-23 14:51:03 +0100

    Update terminal_setup_builder.sh

--- terminal_setup_builder.sh
+++ terminal_setup_builder.sh
@@ -33,6 +33,23 @@ inoremap {<CR> {<CR>}<ESC>O
 inoremap {;<CR> {<CR>};<ESC>O
 nnoremap <buffer> <C-r> <Esc>:w<CR>:exec '!python3' shellescape(@%, 1)<cr>
 
+unction! XTermPasteBegin()
+	  set pastetoggle=<Esc>[201~
+	    set paste
+	      return ""
+      endfunction""]))]"]""]"
+
+" by default, the indent is 2 spaces.
+set shiftwidth=2
+set softtabstop=2
+set tabstop=2
+
+" for yaml files, 2 spaces
+autocmd Filetype yaml setlocal ts=2 sw=2 expandtab
+
+" for python file, 3 spaces
+autocmd Filetype python setlocal ts=3 sw=3 sts=0 expandtab
+
 "Setting the color setup
 color dracula
 set nu
